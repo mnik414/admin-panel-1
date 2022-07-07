7160b2481fe65fee17a57748537b7b89784e4dbd
Shayan <mnik414@gmail.com>
2022-07-07 12:03:01 +0430

    v 0.1

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,55 @@
+import {
+  KeyboardArrowDown,
+  KeyboardArrowUp,
+  MoreVert,
+} from "@mui/icons-material";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import "./featured.scss";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MoreVert fontSize="small" />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
+        </div>
+        <p className="title">Total sales made today</p>
+        <p className="amount">$420</p>
+        <p className="desc">Usage examples for react-circular-progressbar.</p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+
+            <div className="itemResult negative">
+              <KeyboardArrowDown fontSize="small" />
+              <div className="resultAmount">12.4K</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last week</div>
+
+            <div className="itemResult posetive">
+              <KeyboardArrowUp fontSize="small" />
+              <div className="resultAmount">12.4K</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last month</div>
+
+            <div className="itemResult posetive">
+              <KeyboardArrowUp fontSize="small" />
+              <div className="resultAmount">12.4K</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
